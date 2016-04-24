8167d8f8b4a19af41f5738d422fa10bbdab1286d
awetee <awedupe07@gmail.com>
2016-04-24 14:21:24 +0100

    Added services and API Test

---
+++ Tee.FamilyApp/Tee.FamilyApp.Services/IRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tee.FamilyApp.DAL.Entities
+{
+    public interface IRepository<T> : IDisposable where T : class
+    {
+        IEnumerable<T> GetAll();
+
+        T Get(int Id);
+
+        int Add(T entity);
+
+        bool Delete(int id);
+
+        bool Update(T entity);
+    }
+
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public int Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
